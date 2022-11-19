7b619335b2036a340ff35b8f2df412f1f77ea6e7
Богдан Морозюк <bogdanmorozuk72@gmail.com>
2022-11-19 16:30:10 +0200

    added models and base template

--- clothes/store/admin.py
+++ clothes/store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "image",)
+    list_display_links = ("name", "id")
+    search_fields = ("name", "price")
+
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('category',)
+    search_fields = ('category',)
--- clothes/store/models.py
+++ clothes/store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='media/photos')
+    upload_time = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=False, default=1, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+
+
+
+
